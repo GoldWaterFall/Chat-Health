62ba168b1a6a209b8969653266ec44a61e687cba
u7516709 <u7516709@anu.edu.au>
2023-09-05 11:35:17 +1000

    MisUser

---
+++ hospital-api/src/main/java/com/example/hospital/api/db/dao/MisUserDao.java
@@ -0,0 +1,17 @@
+package com.example.hospital.api.db.dao;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+
+public interface MisUserDao {
+    public ArrayList<String> searchUserPermissions(int userId);
+
+    public Integer login(Map param);
+
+
+}
+
+
+
+
---
+++ hospital-api/src/main/java/com/example/hospital/api/service/MisUserService.java
@@ -0,0 +1,8 @@
+package com.example.hospital.api.service;
+
+import java.util.Map;
+
+public interface MisUserService {
+    public Integer login(Map param);
+}
+
