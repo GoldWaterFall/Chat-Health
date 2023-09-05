c59ff0e1698d9c62ef67eecca94c587e10133332
ShiqiDing <62366173+ShiqiDing@users.noreply.github.com>
2023-09-05 10:48:37 +1000

    Delete hospital-api/src/main directory

--- hospital-api/src/main/java/com/example/hospital/api/service/DoctorService.java
+++
@@ -1,9 +0,0 @@
-package com.example.hospital.api.service;
-
-import com.example.hospital.api.common.PageUtils;
-
-import java.util.Map;
-
-public interface DoctorService {
-    public PageUtils searchByPage(Map param);
-}
