f28b4c9c93bf5e85e770e4583679287aa9fb1983
YuSun7543503 <141924871+YuSun7543503@users.noreply.github.com>
2023-10-11 11:29:40 +1100

    Add files via upload

---
+++ Demo Solutions/Demo2/data/README.md
@@ -0,0 +1,1 @@
+This is the store for saving users' unique id and face photos, please make sure you have this data folder before run demo1.py.
---
+++ Demo Solutions/Demo2/demo2_matcher.py
@@ -0,0 +1,26 @@
+import easygui as gui
+import demo2_library
+
+def start():
+    enter = gui.buttonbox(msg='What service do you need?', title='Chat Health platform',
+                          choices=('meet with the doctor', 'quit'))
+    # for face matching
+    if enter == 'meet with the doctor':
+        # enter the unique ID
+        enter_id = gui.enterbox(msg='Please enter your unique ID', title='Unique ID system')
+        if enter_id not in demo2_library.ids():
+            gui.msgbox(msg='Sorry, the unique ID does not exist, please firstly register',
+                       title='Unique ID system', ok_button="OK")
+        else:
+            # compare face to decide whether the same person
+            demo2_library.take_photo(enter_id, 'match')
+            match_result = demo2_library.match('./data/' + enter_id + '/')
+            if match_result:
+                gui.msgbox(msg='You are the same person', title='Face photo collection', ok_button='OK')
+            else:
+                gui.msgbox(msg='You are not the same person', title='Face photo collection', ok_button='OK')
+    # end the use of demo2
+    else:
+        gui.msgbox(msg='The platform service is ending', title='Chat Health platform', ok_button='OK')
+
+
