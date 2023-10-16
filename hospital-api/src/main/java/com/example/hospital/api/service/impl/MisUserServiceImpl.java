40c41bc58c06cb97f7f30a189322a24d2c2c53b9
GoldWater <62366173+GoldWaterFall@users.noreply.github.com>
2023-10-16 23:48:45 +1100

    Update MisUserServiceImpl.java

--- hospital-api/src/main/java/com/example/hospital/api/service/impl/MisUserServiceImpl.java
+++ hospital-api/src/main/java/com/example/hospital/api/service/impl/MisUserServiceImpl.java
@@ -10,9 +10,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.Map;
 
-/**
- * @author ShiqiDing
- */
+
 @Service
 public class MisUserServiceImpl implements MisUserService {
 
